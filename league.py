832d1cd6f71644712034f0e84044df670bb94447
N-Gl <n-gl@gmx.de>
2026-01-11 20:10:00 +0100

    other_historicals_paths, config

--- league.py
+++ league.py
@@ -338,10 +338,16 @@ class MainExploiter(Player):
         if self._payoff[self, opponent] > 0.1 and not self.args.sp:
             return opponent, True
 
-        historical = [
-            player for player in self._payoff.players
-            if isinstance(player, Historical) and player.parent == opponent
-        ]
+        if not self.args.sp:
+            historical = [
+                player for player in self._payoff.players
+                if isinstance(player, Historical) and player.parent == opponent
+            ]
+        else:
+            historical = [
+                player for player in self._payoff.players
+                if isinstance(player, Historical)
+            ]
 
         # args.sp gibt jetzt auch andere Historical as Gegner
         # if self.args.sp:
