9c9f485b288934e1aeb476ac40cd65b2c9d1da52
N-Gl <N-Gl@gmx.de>
2026-01-12 14:20:27 +0100

    config

--- main.py
+++ main.py
@@ -495,6 +495,7 @@ def main(cfg: ExperimentConfig):
             # ["12_12_25__04_12_25__BCagent_update_850", agent_model.Agent, "league_models/12_12_25__04_12_25__BCagent/Main_agent_backups/agent_update_500.pt", None],
             # ["12_12_25__04_12_25__BCagent_update_850", agent_model.Agent, "league_models/12_12_25__04_12_25__BCagent/Main_agent_backups/agent_update_550.pt", None],
             ["finished_PPO_Basis_thesis", agent_model.Agent, "models/finished_PPO_Basis_Thesis/finished_PPO_Basis_thesis.pt", None],
+            ["29_12_25__finished_PPO_Basis_thesis__league__no_training_on_bot_envs_update_250", agent_model.Agent, "league_models/29_12_25__finished_PPO_Basis_thesis__league__no_training_on_bot_envs/Main_agent_backups/agent_update_250.pt", None],
             # ["old_ppo_args__12_12_25__04_12_25__PPO", agent_model.Agent, "league_models/12_12_25__04_12_25__BCagent/Main_agent_backups/agent_update_1710.pt", None],
         ]
         # 2nd element: uninitialized Agent class 
@@ -510,12 +511,12 @@ def main(cfg: ExperimentConfig):
             ("randomAI", microrts_ai.randomAI),
             ("randomBiasedAI", microrts_ai.randomBiasedAI),
             ("rojo", microrts_ai.rojo),
-            ("mixedBot", microrts_ai.mixedBot),
-            ("izanagi", microrts_ai.izanagi), 
-            ("tiamat", microrts_ai.tiamat),
-            ("droplet", microrts_ai.droplet),
-            # ("guidedRojoA3N", microrts_ai.guidedRojoA3N),
-            ("naiveMCTSAI", microrts_ai.naiveMCTSAI),
+            # ("mixedBot", microrts_ai.mixedBot),
+            # ("izanagi", microrts_ai.izanagi), 
+            # ("tiamat", microrts_ai.tiamat),
+            # ("droplet", microrts_ai.droplet),
+            # # ("guidedRojoA3N", microrts_ai.guidedRojoA3N),
+            # ("naiveMCTSAI", microrts_ai.naiveMCTSAI),
         ]
 
         if args.model_path:
