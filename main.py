349cbfb9671a0109ddd3e60bf4390f8034867132
N-Gl <N-Gl@gmx.de>
2026-01-21 11:51:39 +0100

    config

--- main.py
+++ main.py
@@ -504,7 +504,7 @@ def main(cfg: ExperimentConfig):
         default_bot_opponents: Sequence[Tuple[str, Any]] = [
             ("coacAI", microrts_ai.coacAI),
             ("mayari", microrts_ai.mayari),
-            ("passiveAI", microrts_ai.passiveAI),
+            # ("passiveAI", microrts_ai.passiveAI),
             ("workerRushAI", microrts_ai.workerRushAI),
             ("lightRushAI", microrts_ai.lightRushAI),
             ("randomAI", microrts_ai.randomAI),
