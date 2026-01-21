0f7b5b2e71c7b3181271e89b3b5c15b78b14b148
N-Gl <N-Gl@gmx.de>
2026-01-21 16:56:20 +0100

    simplify MainExploiter.get_match

--- league.py
+++ league.py
@@ -413,10 +413,11 @@ class MainExploiter(Player):
             if len(win_rates) and min_win_rate > threshold:
                 rand = np.random.random()
                 if min_win_rate > 0.7:
-                    if rand < 0.6:       # ab min_win_rate = 0.7 -> 60%
+                    if rand < 0.6:       # min_win_rate = 0.7 -> 60%
                         return opponent, True
-                elif rand < (min_win_rate - threshold) * (1/2) + 0.1:       # ab min_win_rate = threshold -> 10%, min_win_rate = 0.7 -> 25%
-                    return opponent, True
+                else:
+                    if rand < 0.2:       # min_win_rate = threshold -> 20%
+                        return opponent, True
 
         # args.sp gibt jetzt auch andere Historical as Gegner
         # if self.args.sp:
