162310e83f8c0163a61dbcae5dc9287291f09314
N-Gl <n-gl@gmx.de>
2026-03-09 11:49:09 +0100

    fix args.unit_bonus

--- selfplay_league.py
+++ selfplay_league.py
@@ -721,19 +721,19 @@ class LeagueTrainer:
                         self.unit_bonus_distr[args.num_selfplay_envs:],
                     )
 
-                    if args.unit_bonus:
-                        sp_score_tensor = self._add_unit_bonus_to_score(
-                            sp_score_tensor,
-                            sp_sc[:, 3:7],
-                            sp_sc[:, 7:11],
-                            self._unit_bonus_max(device)
-                        )
-                        bot_score_tensor = self._add_unit_bonus_to_score(
-                            bot_score_tensor,
-                            bot_sc[:, 3:7],
-                            bot_sc[:, 7:11],
-                            self._unit_bonus_max(device),
-                        )
+                if args.unit_bonus:
+                    sp_score_tensor = self._add_unit_bonus_to_score(
+                        sp_score_tensor,
+                        sp_sc[:, 3:7],
+                        sp_sc[:, 7:11],
+                        self._unit_bonus_max(device)
+                    )
+                    bot_score_tensor = self._add_unit_bonus_to_score(
+                        bot_score_tensor,
+                        bot_sc[:, 3:7],
+                        bot_sc[:, 7:11],
+                        self._unit_bonus_max(device),
+                    )
 
 
 
